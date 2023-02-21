6b7c820f64026acc94a2b718c96b4f507d33d406
noraraasen <91788412+noraraasen@users.noreply.github.com>
2023-02-21 14:51:58 +0100

    Update heart_disease_analysis.r

--- heart_disease_analysis.r
+++ heart_disease_analysis.r
@@ -1,8 +1,32 @@
+---
+subtitle: "MA8701 Advanced Statistical Learning V2023"
+title: "Compulsory exercise: Team Supergreat"
+author: "Nora Aasen, Elias Angelsen, Jonas Nordstrøm"
+date: "`r format(Sys.time(), '%d %B, %Y')`"
+output: 
+  html_document
+  # pdf_document
+---
+  
+```{r setup, include=FALSE}
+library(knitr)
+knitr::opts_chunk$set(echo = TRUE,tidy=TRUE,message=FALSE,warning=FALSE,strip.white=TRUE,prompt=FALSE,
+                      cache=TRUE, size="scriptsize",
+                      fig.width=4, fig.height=3,fig.align = "center")
+```
+
+
+# Setup
+
+```{r Loading Packages,eval=TRUE,echo=FALSE}
 # Load libraries
 library(naniar)
 library(mice)
 library(glmnet)
+```
 
+## Exploratory Analysis
+```{r Loading Data}
 # Load and look at data
 data <- read.csv("framingham.csv")
 head(data)
@@ -15,8 +39,15 @@ dim(df_complete)
 
 # Should we code the binary responses to be categorical instead of integers? 
 # Education should be categorical
+```
+## Missing Data
 
+# Model
+
+## Naive Approach
+```{r Logistic Model}
 # Fit a logistic model
 mod0 <- glm(TenYearCHD ~ ., data = df_complete, family = binomial())
 
 summary(mod0)
+```
